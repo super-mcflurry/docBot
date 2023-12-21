95b34491d2bb7286bd36bd9c54e77bae600149e5
GUAN HONG LAI <101321011+tarucguanhong@users.noreply.github.com>
2023-12-21 23:34:14 +0800

    Update 4_📄_chat_with_your_documents.py

--- pages/4_📄_chat_with_your_documents.py
+++ pages/4_📄_chat_with_your_documents.py
@@ -1,127 +1,108 @@
-import os
-import utils
 import streamlit as st
-from streaming import StreamHandler
 from dotenv import load_dotenv
-
-from langchain.document_loaders import PyPDFLoader
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
+from langchain.vectorstores import FAISS
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.chains import ConversationalRetrievalChain
-from langchain.vectorstores import DocArrayInMemorySearch
-
-
-from langchain.chat_models import ChatOpenAI
+from htmlTemplates import css, bot_template, user_template
 from langchain.llms import HuggingFaceHub
-from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain.embeddings import OpenAIEmbeddings,  HuggingFaceEmbeddings 
-from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain.chat_models import ChatOpenAI
+
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
 
 
-from streamlit_mic_recorder import speech_to_text
+def get_text_chunks(text):
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len
+    )
+    chunks = text_splitter.split_text(text)
+    return chunks
 
-st.set_page_config(page_title="ChatPDF", page_icon="📄")
-st.header('Chat with your documents')
 
-class Chatbot:
+def get_vectorstore(text_chunks):
+    embeddings = OpenAIEmbeddings()
+    #embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+    # embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
+    vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
+    return vectorstore
 
-    def download_file(self,file):
-        folder = "downloads"
-        if not os.path.exists(folder):
-            os.makedirs(folder)
-        
-        file_path = f'./{folder}/{file.name}'
-        with open(file_path, 'wb') as f:
-            f.write(file.getvalue())
-        return file_path
-    
-    def get_text(self, uploaded_files):
-        docs = []
-        for file in uploaded_files:
-            file_path = self.download_file(file)
-            pdf_loader = PyPDFLoader(file_path)
-            docs.extend(pdf_loader.load())
-        return docs
 
-    def split_text(self, file):
-        text_splitter = RecursiveCharacterTextSplitter(
-            chunk_size=1500,
-            chunk_overlap=200
-        )
-        chunks = text_splitter.split_documents(file)
-        return chunks
-    
-    def embeddings_vectorstore(self, chunks):
-       embeddings = OpenAIEmbeddings()
-       # embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
-       # embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+def get_conversation_chain(vectorstore):
+    #llm = ChatGoogleGenerativeAI(model="gemini-pro", temperature=0.7,convert_system_message_to_human=True)
+    llm = ChatOpenAI(model_name="gpt-3.5-turbo-1106", temperature=0.7)
+    # llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
 
-       vectorstore = DocArrayInMemorySearch.from_documents(chunks, embeddings)
-       return vectorstore
-       
-    def conversation_chain(self,vectorstore):
+    memory = ConversationBufferMemory(
+        memory_key='chat_history', return_messages=True)
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        retriever=vectorstore.as_retriever(),
+        memory=memory
+    )
+    return conversation_chain
 
-        memory = ConversationBufferMemory(
-            memory_key='chat_history',
-            return_messages=True
-        )
 
-        retriever = vectorstore.as_retriever(
-            search_type='mmr',
-            search_kwargs={'k':2, 'fetch_k':4}
-        )
-        
-        llm = ChatOpenAI(model_name="gpt-3.5-turbo-1106", streaming=True)
-        # llm = ChatGoogleGenerativeAI(model="gemini-pro", temperature=0.7,convert_system_message_to_human=True)
-        # llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
- 
-        chain = ConversationalRetrievalChain.from_llm(llm, retriever=retriever, memory=memory, verbose=True)
+def handle_userinput(user_question):
+    response = st.session_state.conversation({'question': user_question})
+    st.session_state.chat_history = response['chat_history']
 
-        return chain
-    
-    @utils.enable_chat_history
-    def main(self):
-        load_dotenv()
-        with st.sidebar:
-            st.subheader("Upload your Documents")
-            uploaded_files = st.file_uploader(label='Upload PDF files', type=['pdf'], accept_multiple_files=True)
-        if not uploaded_files:
-            st.error("Please upload PDF documents to continue!")
-            st.stop()
+    for i, message in enumerate(st.session_state.chat_history):
+        if i % 2 == 0:
+            st.write(user_template.replace(
+                "{{MSG}}", message.content), unsafe_allow_html=True)
+        else:
+            st.write(bot_template.replace(
+                "{{MSG}}", message.content), unsafe_allow_html=True)
 
-        # Chatbot
-        if uploaded_files:
+
+def main():
+    load_dotenv()
+    st.set_page_config(page_title="Chat with multiple PDFs",
+                       page_icon=":books:")
+    st.write(css, unsafe_allow_html=True)
+
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = None
+
+    st.header("Chat with multiple PDFs :books:")
+    user_question = st.text_input("Ask a question about your documents:")
+    if user_question:
+        handle_userinput(user_question)
+
+    with st.sidebar:
+        st.subheader("Your documents")
+        pdf_docs = st.file_uploader(
+            "Upload your PDFs here and click on 'Process'", accept_multiple_files=True)
+        if st.button("Process"):
             with st.spinner("Processing"):
-                docs = self.get_text(uploaded_files)
-                chunks = self.split_text(docs)
-                vectorstore = self.embeddings_vectorstore(chunks)
-                qa_chain = self.conversation_chain(vectorstore)
+                # get pdf text
+                raw_text = get_pdf_text(pdf_docs)
 
-        # Display chat input
-        user_query = st.chat_input(placeholder="Ask me anything!", key="user_input")
+                # get the text chunks
+                text_chunks = get_text_chunks(raw_text)
 
-        # Speech-to-Text
-        c1, c2 = st.columns(2)
-        with c1:
-            st.write("Convert speech to text:")
-        with c2:
-            text = speech_to_text(language='en', use_container_width=True, just_once=True, key='STT')
+                # create vector store
+                vectorstore = get_vectorstore(text_chunks)
 
-        # Update chat input with the latest speech-to-text result
-        st.session_state.text_received = text
+                # create conversation chain
+                st.session_state.conversation = get_conversation_chain(
+                    vectorstore)
 
-        if text:
-            user_query = text
 
-        if user_query:
-            utils.display_msg(user_query, 'user')
-
-            with st.chat_message("assistant"):
-                st_cb = StreamHandler(st.empty())
-                response = qa_chain.run(user_query, callbacks=[st_cb])
-                st.session_state.messages.append({"role": "assistant", "content": response})
-
-if __name__ == "__main__":
-    obj = Chatbot()
-    obj.main()
+if __name__ == '__main__':
+    main()
