cac6eb7c6ccf36756f06468694f87c6f4cc386f8
GUAN HONG LAI <101321011+tarucguanhong@users.noreply.github.com>
2023-12-25 20:02:08 +0800

    Update 2_📊CSV_ChatBot.py

--- pages/2_📊CSV_ChatBot.py
+++ pages/2_📊CSV_ChatBot.py
@@ -2,7 +2,6 @@ import streamlit as st
 from dotenv import load_dotenv
 from langchain.agents.agent_types import AgentType
 from langchain.chat_models import ChatOpenAI
-from langchain.llms import OpenAI
 from langchain_experimental.agents.agent_toolkits import create_csv_agent
 
 from streamlit_mic_recorder import speech_to_text
